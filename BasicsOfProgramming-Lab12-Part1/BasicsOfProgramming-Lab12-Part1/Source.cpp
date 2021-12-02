966ed8c4f06b0e785be2f770c17f41ebfc5ae37d
SpectreSpect <spectrefromthefuture@gmail.com>
2021-12-02 22:29:16 +0400

    First commit

---
+++ BasicsOfProgramming-Lab12-Part1/BasicsOfProgramming-Lab12-Part1/Source.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#define TEXT_SIZE 256
+struct Inscription
+{
+	wchar_t text[TEXT_SIZE];
+	int x, y;
+	char R, G, B;
+	int fontSize;
+};
+
+int main()
+{
+	unsigned int figuresCount = 0;
+
+	printf("Enter figures count:");
+	scanf_s("%d", &figuresCount);
+
+	Inscription* inscription = (Inscription*)malloc(sizeof(Inscription) * figuresCount);
+
+	for (int i = 0; i < figuresCount; i++)
+	{
+		printf("\nENTERING PROPERTIES FOR FIGURE %d\n", i);
+		
+		printf("Enter text for inscription:");
+		wscanf_s(L" %[^\t\n]s", inscription[i].text, TEXT_SIZE);
+		
+		printf("Enter inscription x position:");
+		scanf_s("%d", &inscription[i].x);
+
+		printf("Enter inscription y position:");
+		scanf_s("%d", &inscription[i].y);
+
+		printf("Enter inscription R color:");
+		scanf_s("%d", &inscription[i].R);
+
+		printf("Enter inscription G color:");
+		scanf_s("%d", &inscription[i].G);
+
+		printf("Enter inscription B color:");
+		scanf_s("%d", &inscription[i].B);
+
+		printf("Enter inscription font size:");
+		scanf_s("%d", &inscription[i].fontSize);
+	}
+
+	FILE* filePointer = nullptr;
+	fopen_s(&filePointer, "data.txt", "wb");
+	fwrite(&figuresCount, sizeof(int), 1, filePointer);
+	fwrite(inscription, sizeof(Inscription), figuresCount, filePointer);
+	fclose(filePointer);
+
+	//unsigned int inscriptionsCount = 0;
+	//fopen_s(&filePointer, "data.txt", "rb");
+	//fread(&inscriptionsCount, sizeof(int), 1, filePointer);
+	//Inscription* inscriptions = (Inscription*)malloc(sizeof(Inscription) * inscriptionsCount);
+	//fread(inscriptions, sizeof(Inscription), inscriptionsCount, filePointer);
+
+	fclose(filePointer);
+
+}
---
+++ BasicsOfProgramming-Lab12-Part2/BasicsOfProgramming-Lab12-Part2/BasicsOfProgramming-Lab12-Part2.cpp
@@ -0,0 +1,208 @@
+﻿#include "framework.h"
+#include "BasicsOfProgramming-Lab12-Part2.h"
+#include <stdio.h>
+#include <tchar.h>
+
+#define MAX_LOADSTRING 100
+
+HINSTANCE hInst;        
+WCHAR szTitle[MAX_LOADSTRING];      
+WCHAR szWindowClass[MAX_LOADSTRING];           
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_BASICSOFPROGRAMMINGLAB12PART2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_BASICSOFPROGRAMMINGLAB12PART2));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BASICSOFPROGRAMMINGLAB12PART2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_BASICSOFPROGRAMMINGLAB12PART2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+#define TEXT_SIZE 256
+struct Inscription
+{
+    wchar_t text[TEXT_SIZE];
+    int x, y;
+    char R, G, B;
+    int fontSize;
+};
+
+void LoadInscriptions(const char* filePath, Inscription*& inscriptions, unsigned int& inscriptionsCount, HFONT*& hFont)
+{
+    FILE* filePointer;
+    fopen_s(&filePointer, filePath, "rb");
+
+    fread(&inscriptionsCount, sizeof(int), 1, filePointer);
+
+    inscriptions = (Inscription*)malloc(sizeof(Inscription) * inscriptionsCount);
+    fread(inscriptions, sizeof(Inscription), inscriptionsCount, filePointer);
+
+    fclose(filePointer);
+
+    hFont = (HFONT*)malloc(sizeof(HFONT) * inscriptionsCount);
+    for (int i = 0; i < inscriptionsCount; i++)
+        hFont[i] = CreateFont(inscriptions[i].fontSize, 0, 0, 0, 0, 0, 0, 0, DEFAULT_CHARSET, 0, 0, 0, 0, L"Courier New");
+}
+
+Inscription* inscriptions = nullptr;
+unsigned int inscriptionsCount = 0;
+HFONT* hFonts = nullptr;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        {
+            LoadInscriptions("data.txt", inscriptions, inscriptionsCount, hFonts);
+        }
+    case WM_COMMAND:
+        {
+
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            for (int i = 0; i < inscriptionsCount; i++)
+            {
+                SelectObject(hdc, hFonts[i]);
+                SetTextColor(hdc, RGB(inscriptions[i].R, inscriptions[i].G, inscriptions[i].B));
+                TextOut(hdc, inscriptions[i].x, inscriptions[i].y, (LPCWSTR)inscriptions[i].text, _tcslen(inscriptions[i].text));
+            }
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
